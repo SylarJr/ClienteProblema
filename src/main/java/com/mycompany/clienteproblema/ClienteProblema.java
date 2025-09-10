a279d4990943ce545ac45f251aaef107d8459449
Sylar490 <algandar490@gmail.com>
2025-09-09 20:45:47 -0700

    first commit

---
+++ src/main/java/com/mycompany/clienteproblema/ClienteProblema.java
@@ -0,0 +1,33 @@
+
+package com.mycompany.clienteproblema;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ClienteProblema {
+
+    public static void main(String[] args) throws IOException {
+       
+        
+       Socket salida = new Socket("localhost", 8080);
+        PrintWriter escritor = new PrintWriter(salida.getOutputStream(), true);
+        BufferedReader lector = new BufferedReader(new InputStreamReader(salida.getInputStream()));
+        BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+        String cadena = teclado.readLine();
+        String mensaje;
+        
+        while (!cadena.equalsIgnoreCase("fin")) {
+            escritor.println(cadena);
+            mensaje = lector.readLine();
+            System.out.println(mensaje);
+            cadena = teclado.readLine();
+        }
+        salida.close();
+
+    
+    }
+}
